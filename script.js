016c745c7bd4e2632f5c42a6c0c2e923c7151862
Rishu Kumar <126085066+rishu-7549@users.noreply.github.com>
2025-02-10 22:47:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+function updateSize() {
+    const size = document.getElementById('sizeInfo');
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    size.querySelector('h1').textContent = `Width: ${width} and Height: ${height}`;
+}
+
+// Call the function on page load
+updateSize();
+
+// Add event listener for window resize
+window.addEventListener('resize', updateSize);
